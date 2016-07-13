b0a90d34217a9a257789928791999814e72b2f2f
Gary Marigliano <gmarigliano93@gmail.com>
2016-07-13 19:23:29 +0200

    Add some todo in run_stow.sh

--- run_stow.sh
+++ run_stow.sh
@@ -2,3 +2,7 @@
 echo "Stowing..."
 stow xfce4-terminal -t ~/.config/xfce4/terminal/
 echo "TODO add the others stow commands"
+TODO foreach folder do
+stow -v FOLDER_NAME
+
+TODO create folder structure into each "stow app"
