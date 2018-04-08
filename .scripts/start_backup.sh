dd47f07bd6aa0503218cd0e6ad3af151c0bef92b
Gary Marigliano <gmarigliano93@gmail.com>
2018-04-08 22:14:34 +0200

    remove i3lock-fancy, old backup script and onedrived script

--- .scripts/start_backup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-if [[ ! -f /tmp/.already_backup-up ]] && [[ "$(iwgetid | grep wifimaison)" != "" ]]; then
-    deja-dup --backup
-    touch /tmp/.already_backed-up
-fi
-
--- .scripts/start_onedrived.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-sleep 30
-nohup onedrive-d start --debug &> /var/log/onedrive_d.log &
-sleep 2
-onedrive-d-systemtray
