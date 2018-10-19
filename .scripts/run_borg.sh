08adac69dd36f935ab7c926c4c116c2a45eb29d2
Gary Marigliano <gmarigliano93@gmail.com>
2018-10-19 17:50:20 +0200

    script: move borg location

--- .scripts/run_borg.sh
+++ .scripts/run_borg.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/home/gary/Dropbox/borg/borg_backup_script.sh 2>&1 | p3mail "[Borg] backup for `date +'%F %T'`"
+/home/gary/scripts/borg_backup_script.sh 2>&1 | p3mail "[Borg] backup for `date +'%F %T'`"
