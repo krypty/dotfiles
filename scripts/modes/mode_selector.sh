789459bcf94dfc9d459af65545c77385c1bf25f7
Gary Marigliano <gmarigliano93@gmail.com>
2018-12-28 14:54:46 +0100

    modes: fix dirname path

--- scripts/modes/mode_selector.sh
+++ scripts/modes/mode_selector.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-modes_folder="/home/gary/dotfiles/scripts/modes/modes"
-mode=$(ls -1 $modes_folder | grep -vE ".sh$" | rofi -dmenu)
+cdir=$(dirname $0)
+mode=$(ls -1 $cdir/modes | grep -vE ".sh$" | rofi -dmenu)
 if [ -z "$mode" ] ; then
         exit 0
 fi
 
 notify-send $mode
-bash modes/$mode.sh
+bash $cdir/modes/$mode.sh
--- scripts/modes/modes/34inches.sh
+++ scripts/modes/modes/34inches.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+cdir=$(dirname $0)
+
 mons -s
 i3-msg restart
 sleep 3
-feh --bg-scale /home/gary/dotfiles/scripts/modes/modes/34inches/wallpaper
+# feh --bg-scale /home/gary/dotfiles/scripts/modes/modes/34inches/wallpaper
+feh --bg-scale $cdir/34inches/wallpaper
 notify-send "Mode set to 34inches"
