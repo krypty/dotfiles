6d25ccef608ed50a3c352b6837fe2bdd6176ca6d
Gary Marigliano <gary.marigliano@ecorobotix.com>
2019-07-15 08:06:35 +0200

    bash: add git aliases

--- rc_files/.rc_files/aliases/git-aliases.sh
+++ rc_files/.rc_files/aliases/git-aliases.sh
@@ -1,7 +1,8 @@
 alias git='LANG=en_US.utf-8 git'
 alias g=git
 alias gst="git status"
-alias gd="git difftool"
-alias gds="git difftool --staged"
+alias gd="git diff"
+alias gds="git diff --staged"
 alias lg="lazygit"
 alias gg="gitg"
+alias gco="git checkout"
