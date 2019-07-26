4e50300959ecf27c6d7b2f59ca1476c939d769b9
Gary Marigliano <gmarigliano93@gmail.com>
2019-07-26 16:46:17 +0200

    bash: fix menu-complete

--- rc_files/.rc_files/input/bash_input.sh
+++ rc_files/.rc_files/input/bash_input.sh
@@ -1,7 +1,10 @@
 # Better menu complete
 if [ "$TERM" = xterm ]; then TERM=xterm-256color; fi
 bind 'set show-all-if-ambiguous on'
-bind 'TAB:menu-complete'
+bind 'set show-all-if-unmodified on'
+bind 'set menu-complete-display-prefix on'
+bind '"\t": menu-complete'
+bind '"\e[Z": menu-complete-backward'
 
 # Disable terminal freeze after pressing ctrl+s
 stty -ixon
