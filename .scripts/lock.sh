fd5336677db81efd84e0ff296bf5ac4591559dd6
Gary Marigliano <gary.marigliano@ecorobotix.com>
2020-01-13 08:39:10 +0100

    i3lock: simplify lock style

--- .scripts/lock.sh
+++ .scripts/lock.sh
@@ -2,8 +2,8 @@
 
 # Enable compton's fade-in effect so that the lockscreen gets a nice fade-in
 # effect.
-dbus-send --print-reply --dest=com.github.chjj.compton.${DISPLAY/:/_} / \
-    com.github.chjj.compton.opts_set string:no_fading_openclose boolean:false
+# dbus-send --print-reply --dest=com.github.chjj.compton.${DISPLAY/:/_} / \
+#     com.github.chjj.compton.opts_set string:no_fading_openclose boolean:false
 
 # If disable unredir_if_possible is enabled in compton's config, we may need to
 # disable that to avoid flickering. YMMV. To try that, uncomment the following
@@ -13,17 +13,18 @@ dbus-send --print-reply --dest=com.github.chjj.compton.${DISPLAY/:/_} / \
 
 # Suspend dunst and lock, then resume dunst when unlocked.
 # pkill -u $USER -USR1 dunst
-i3lock -n -i $HOME/Pictures/wall.jpg \
-    --insidecolor=373445ff --ringcolor=ffffffff --line-uses-inside \
-    --keyhlcolor=d23c3dff --bshlcolor=d23c3dff --separatorcolor=00000000 \
-    --insidevercolor=fecf4dff --insidewrongcolor=d23c3dff \
-    --ringvercolor=ffffffff --ringwrongcolor=ffffffff --indpos="x+86:y+1003" \
-    --radius=15 --veriftext="" --wrongtext=""
+i3lock-fancy
+# i3lock-fancy -n -i $HOME/Pictures/wall.jpg \
+#     --insidecolor=373445ff --ringcolor=ffffffff --line-uses-inside \
+#     --keyhlcolor=d23c3dff --bshlcolor=d23c3dff --separatorcolor=00000000 \
+#     --insidevercolor=fecf4dff --insidewrongcolor=d23c3dff \
+#     --ringvercolor=ffffffff --ringwrongcolor=ffffffff --indpos="x+86:y+1003" \
+#     --radius=15 --veriftext="" --wrongtext=""
 # pkill -u $USER -USR2 dunst
 
 # Revert compton's config changes.
 sleep 0.2
-dbus-send --print-reply --dest=com.github.chjj.compton.${DISPLAY/:/_} / \
-    com.github.chjj.compton.opts_set string:no_fading_openclose boolean:true
+# dbus-send --print-reply --dest=com.github.chjj.compton.${DISPLAY/:/_} / \
+#     com.github.chjj.compton.opts_set string:no_fading_openclose boolean:true
 # dbus-send --print-reply --dest=com.github.chjj.compton.${DISPLAY/:/_} / \
 #     com.github.chjj.compton.opts_set string:unredir_if_possible boolean:true
