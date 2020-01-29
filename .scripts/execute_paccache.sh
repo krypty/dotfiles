1d863c06f883330fb55165ecc4b6d989fef8a352
Gary Marigliano <gmarigliano93@gmail.com>
2020-01-29 21:17:32 +0100

    [pacman] reduce number of backed up packages by paccache

--- .scripts/execute_paccache.sh
+++ .scripts/execute_paccache.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 # Put that script in /etc/pacman.d/hooks/ and make it executable
-/usr/bin/paccache -vrk3
+/usr/bin/paccache -vrk2
 /usr/bin/paccache -vruk1
-/usr/bin/paccache --cachedir /home/gary/.cache/pikaur/pkg/ -vrk3
+/usr/bin/paccache --cachedir /home/gary/.cache/pikaur/pkg/ -vrk2
 /usr/bin/paccache --cachedir /home/gary/.cache/pikaur/pkg/ -vruk1
-date > /tmp/yolo
