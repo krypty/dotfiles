d6bb74329c09104bc05dc7c1d07a6b309cd6c785
Gary Marigliano <gmarigliano93@gmail.com>
2021-07-27 15:26:18 +0200

    [nnn] Add alias to n

--- rc_files/.rc_files/default.sh
+++ rc_files/.rc_files/default.sh
@@ -59,4 +59,9 @@ export FZF_DEFAULT_OPTS="
 
 }
 
+export NNN_PLUG='f:finder;o:fzopen;p:mocplay;d:diffs;t:nmount;v:imgview'
+BLK="04" CHR="04" DIR="04" EXE="00" REG="00" HARDLINK="00" SYMLINK="06" MISSING="00" ORPHAN="01" FIFO="0F" SOCK="0F" OTHER="02"
+export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$OTHER"
+
+
 _gen_fzf_default_opts
