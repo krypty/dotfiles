d48601889554a009c94cac987cbb8de4e5d6a76c
Gary Marigliano <gmarigliano93@gmail.com>
2021-08-18 13:12:58 +0200

    [nnn] Show hidden files by default

--- rc_files/.rc_files/default.sh
+++ rc_files/.rc_files/default.sh
@@ -62,6 +62,7 @@ export FZF_DEFAULT_OPTS="
 export NNN_PLUG='f:finder;o:fzopen;p:mocplay;d:diffs;t:nmount;v:imgview'
 BLK="04" CHR="04" DIR="04" EXE="00" REG="00" HARDLINK="00" SYMLINK="06" MISSING="00" ORPHAN="01" FIFO="0F" SOCK="0F" OTHER="02"
 export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$OTHER"
+export NNN_OPTS="H"
 
 
 export RIPGREP_CONFIG_PATH=~/.ripgreprc
