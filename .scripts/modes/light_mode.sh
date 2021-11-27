10c8233153671637548b9230f4ae2030d4681974
Gary Marigliano <github@marigliano.ch>
2021-11-27 15:41:11 +0100

    [mode] Improve light mode

--- .scripts/modes/light_mode.sh
+++ .scripts/modes/light_mode.sh
@@ -1,2 +1,5 @@
 #/usr/bin/env bash
-sed -i 's|colors: \*light|colors: *dark|' ~/.config/alacritty/alacritty.yml
+sed -i 's|colors: \*dark|colors: *light|' ~/.config/alacritty/alacritty.yml
+
+sed -i "s|set -g @tmux-gruvbox 'dark'|set -g @tmux-gruvbox 'light'|" ~/.tmux.conf
+tmux source-file ~/.tmux.conf
