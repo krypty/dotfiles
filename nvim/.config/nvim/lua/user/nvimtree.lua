be672045562db8576efa231deafecd821de2d4a8
Gary Marigliano <gmarigliano93@gmail.com>
2023-09-06 15:06:41 +0200

    [nvim] Fix nvimtree update

--- nvim/.config/nvim/lua/user/nvimtree.lua
+++ nvim/.config/nvim/lua/user/nvimtree.lua
@@ -1,26 +1,12 @@
--- following options are the default
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
-local status_ok, nvim_tree = pcall(require, "nvim-tree")
-if not status_ok then
-    return
-end
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-    return
-end
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
-
-nvim_tree.setup {
-    disable_netrw = true,
-    hijack_netrw = true,
-    --[[ ignore_ft_on_setup = { ]]
-    --[[     "startify", ]]
-    --[[     "dashboard", ]]
-    --[[     "alpha", ]]
-    --[[ }, ]]
+-- empty setup using defaults
+require("nvim-tree").setup({
     hijack_cursor = false,
     update_cwd = true,
     hijack_directories = {
@@ -36,41 +22,8 @@ nvim_tree.setup {
             error = "",
         },
     },
-    update_focused_file = {
-        enable = true,
-        update_cwd = false,
-        ignore_list = {},
-    },
-    git = {
-        enable = true,
-        ignore = true,
-        timeout = 500,
-    },
-    view = {
-        width = 30,
-        hide_root_folder = false,
-        side = "left",
-        --[[ mappings = { ]]
-        --[[     custom_only = false, ]]
-        --[[     list = { ]]
-        --[[     { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" }, ]]
-        --[[     { key = "h", cb = tree_cb "close_node" }, ]]
-        --[[     { key = "v", cb = tree_cb "vsplit" }, ]]
-        --[[     }, ]]
-        --[[ }, ]]
-        number = false,
-        relativenumber = false,
-    },
-    actions = {
-        open_file = {
-          quit_on_open = false,
-          window_picker = { enable = true },
-          resize_window = true,
-        },
-    },
     renderer = {
         highlight_git = true,
-        root_folder_modifier = ":t",
         icons = {
             show = {
                 file = true,
@@ -101,4 +54,4 @@ nvim_tree.setup {
         }
     }
 }
-
+)
