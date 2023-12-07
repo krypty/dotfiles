52362f5036ee7717974811b59f956ae69e56c051
Gary Marigliano <github@marigliano.ch>
2023-12-07 14:39:10 +0100

    [rc_files] Add pyenv support if installed via pyenv-installer

--- rc_files/.rc_files/default.sh
+++ rc_files/.rc_files/default.sh
@@ -94,8 +94,10 @@ if [[ -z "$SSH_AUTH_SOCK" ]]; then
 fi
 
 # pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
 if which pyenv >/dev/null 2>&1; then
-  export PYENV_ROOT="$HOME/.pyenv"
   command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
   eval "$(pyenv init -)"
   eval "$(pyenv virtualenv-init -)"
