9ea585d20db546e7b5f9f8c95c63d22ae8b81a6d
Gary Marigliano <gmarigliano93@gmail.com>
2024-09-11 18:09:01 +0200

    [alias] rename youtube-dl to yt-dlp

--- rc_files/.rc_files/aliases/youtube-dl-aliases.sh
+++ rc_files/.rc_files/aliases/youtube-dl-aliases.sh
@@ -1,3 +1,3 @@
-alias youtube-dl-playlist="youtube-dl --all-subs -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' "
-alias youtube-dl-mp3="youtube-dl -o '%(title)s-%(id)s.%(ext)s' -x --audio-format mp3 --audio-quality 0"
-alias youtube-dl-playlist-mp3="youtube-dl-playlist -x --audio-format=mp3 --audio-quality=0 "
+alias yt-dlp-playlist="yt-dlp --all-subs -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' "
+alias yt-dlp-mp3="yt-dlp -o '%(title)s-%(id)s.%(ext)s' -x --audio-format mp3 --audio-quality 0"
+alias yt-dlp-playlist-mp3="yt-dlp-playlist -x --audio-format=mp3 --audio-quality=0 "
