e18465bec5bfc900f2bc3a5ee1f5181420499e7b
Gary Marigliano <gmarigliano93@gmail.com>
2025-02-02 11:07:40 +0100

    wezterm: use it as default

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,43 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.color_scheme = 'Gruvbox light, medium (base16)'
+config.color_scheme = 'Gruvbox (Gogh)'
+
+
+config.enable_tab_bar = false
+
+config.window_padding = {
+    left = 2,
+    right = 2,
+    top = 0,
+    bottom = 0,
+}
+
+-- Key bindings for increasing and decreasing font size
+config.keys = {
+    {
+        key = 'PageUp',
+        mods = 'ALT',
+        action = wezterm.action.IncreaseFontSize,
+    },
+    {
+        key = 'PageDown',
+        mods = 'ALT',
+        action = wezterm.action.DecreaseFontSize,
+    },
+    {
+        key = '0',
+        mods = 'ALT',
+        action = wezterm.action.ResetFontSize,
+    },
+};
+
+-- and finally, return the configuration to wezterm
+return config
