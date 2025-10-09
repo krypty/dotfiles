4726b2058a6db1e23290dc9999f33248b869526f
Gary Marigliano <gmarigliano93@gmail.com>
2025-10-10 01:36:37 +0200

    wezterm: use wayland (custom build)

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -17,34 +17,50 @@ config.window_close_confirmation = "NeverPrompt"
 config.enable_tab_bar = false
 
 -- FIXME: remove this
-config.enable_wayland = false
+-- custom build: I reverted https://github.com/wezterm/wezterm/pull/6508/files
+-- config.enable_wayland = false
 
 config.window_padding = {
-    left = 2,
-    right = 2,
-    top = 0,
-    bottom = 0,
+	left = 2,
+	right = 2,
+	top = 0,
+	bottom = 0,
 }
 
-config.font_size = 11.0
+config.font_size = 12
+config.font = wezterm.font_with_fallback({
+	"DejaVu Sans Mono",
+})
+config.font_rules = {
+	{
+		intensity = "Bold",
+		italic = false,
+		font = wezterm.font("DejaVu Sans Mono", { weight = "Bold", stretch = "Normal", style = "Normal" }),
+	},
+	{
+		intensity = "Bold",
+		italic = true,
+		font = wezterm.font("DejaVu Sans Mono", { weight = "Bold", stretch = "Normal", style = "Italic" }),
+	},
+}
 
 -- Key bindings for increasing and decreasing font size
 config.keys = {
-    {
-        key = "PageUp",
-        mods = "ALT",
-        action = wezterm.action.IncreaseFontSize,
-    },
-    {
-        key = "PageDown",
-        mods = "ALT",
-        action = wezterm.action.DecreaseFontSize,
-    },
-    {
-        key = "0",
-        mods = "ALT",
-        action = wezterm.action.ResetFontSize,
-    },
+	{
+		key = "PageUp",
+		mods = "ALT",
+		action = wezterm.action.IncreaseFontSize,
+	},
+	{
+		key = "PageDown",
+		mods = "ALT",
+		action = wezterm.action.DecreaseFontSize,
+	},
+	{
+		key = "0",
+		mods = "ALT",
+		action = wezterm.action.ResetFontSize,
+	},
 }
 
 local act = wezterm.action
@@ -57,11 +73,11 @@ config.disable_default_mouse_bindings = true
 --
 -- Make Shift-Click open hyperlinks (Shift is implied)
 config.mouse_bindings = {
-    {
-        event = { Up = { streak = 1, button = "Left" } },
-        mods = "NONE",
-        action = act.OpenLinkAtMouseCursor,
-    },
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "NONE",
+		action = act.OpenLinkAtMouseCursor,
+	},
 }
 
 -- and finally, return the configuration to wezterm
