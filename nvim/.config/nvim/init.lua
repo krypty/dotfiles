dd9ea55615d94c2df2c6e3077040cb0dd7080acf
Gary Marigliano <github@marigliano.ch>
2025-10-14 10:23:53 +0200

    nvim: disable autoformat on save

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -781,20 +781,25 @@ require('lazy').setup({
     },
     opts = {
       notify_on_error = false,
+      -- Disable autoformat
       format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
+        return nil
       end,
+      -- Enable autoformat based on filetype
+      -- format_on_save = function(bufnr)
+      --   -- Disable "format_on_save lsp_fallback" for languages that don't
+      --   -- have a well standardized coding style. You can add additional
+      --   -- languages here or re-enable it for the disabled ones.
+      --   local disable_filetypes = { c = true, cpp = true }
+      --   if disable_filetypes[vim.bo[bufnr].filetype] then
+      --     return nil
+      --   else
+      --     return {
+      --       timeout_ms = 500,
+      --       lsp_format = 'fallback',
+      --     }
+      --   end
+      -- end,
       formatters_by_ft = {
         java = { 'google-java-format' },
         lua = { 'luals' },
