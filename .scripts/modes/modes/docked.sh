88167f3bed8a52e91a7595e48ba4be1139a5cae8
Gary Marigliano <github@marigliano.ch>
2026-05-04 13:10:57 +0200

    Add kanshi

---
+++ .scripts/modes/modes/docked.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+killall waybar; waybar
+
+# Assign workspaces 1–5 to DP-3
+for i in 1 2 3 4 5; do
+    swaymsg "workspace $i; move workspace to output DP-3"
+done
+
+# Assign workspaces 6–10 to DP-4
+for i in 6 7 8 9 10; do
+    swaymsg "workspace $i; move workspace to output DP-4"
+done
+
+swaybg -o DP-3 -i ~/Pictures/left.jpg -m fill &
+swaybg -o DP-4 -i ~/Pictures/right.jpg -m fill &
---
+++ .scripts/modes/modes/laptop.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+killall waybar; waybar
+
+for i in 1 2 3 4 5 6 7 8 9 10; do
+    swaymsg "workspace $i; move workspace to output eDP-1"
+done
+
+swaybg -o eDP-1 -i ~/Pictures/1037-1920x1200.jpg -m fill &
